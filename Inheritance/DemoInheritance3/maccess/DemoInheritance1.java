03107f03123f07e895a8e5d69858b40e4cf2c726
AnilAzad <aniliazad@gmail.com>
2022-08-11 17:46:51 +0530

    Static block/Object call

---
+++ Inheritance/DemoInheritance3/maccess/DemoInheritance1.java
@@ -0,0 +1,11 @@
+package maccess;
+
+import test.*;
+
+public class DemoInheritance1 {
+    public static void main(String[] args) {
+        CClass ob = new CClass();
+        ob.m1();
+        ob.m2();
+    }
+}
